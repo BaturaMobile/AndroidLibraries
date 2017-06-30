59e67bbc12d9558295bcb191388836ae82daf8ff
DESKTOP-KARODAP\vssnake <unai@baturamobile.com>
2017-06-30 14:51:12 +0200

    no message

--- mvp/src/main/java/com/baturamobile/mvp/LocationModule.java
+++ mvp/src/main/java/com/baturamobile/mvp/LocationModule.java
@@ -12,7 +12,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import com.baturamobile.design.Utils;
 import com.baturamobile.mvp.storage.PreferencesManager;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
