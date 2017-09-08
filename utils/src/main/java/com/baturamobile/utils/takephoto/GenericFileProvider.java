e201abcbcd2508676737525e24388251fb0c9842
DESKTOP-KARODAP\vssnake <unai@baturamobile.com>
2017-09-08 15:05:39 +0200

    remove library and manual import to take photo

---
+++ utils/src/main/java/com/baturamobile/utils/takephoto/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package com.baturamobile.utils.takephoto;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by vssnake on 08/09/2017.
+ */
+
+public class GenericFileProvider extends FileProvider {
+}
