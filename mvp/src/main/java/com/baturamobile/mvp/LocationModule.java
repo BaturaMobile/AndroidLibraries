d1a7c484d40df9f6b5b8fc9812a32c3038495c23
DESKTOP-6ETPTQV\virtu <unai@baturamobile.com>
2017-11-15 15:46:58 +0100

    location module fix

--- mvp/src/main/java/com/baturamobile/mvp/LocationModule.java
+++ mvp/src/main/java/com/baturamobile/mvp/LocationModule.java
@@ -272,6 +272,7 @@ public class LocationModule implements GoogleApiClient.OnConnectionFailedListene
                 LocationServices.getSettingsClient(baseActivity).checkLocationSettings(locationSettingsRequest);
 
         task.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onComplete(Task<LocationSettingsResponse> task) {
                 try {
@@ -282,7 +283,7 @@ public class LocationModule implements GoogleApiClient.OnConnectionFailedListene
                     try{
                         LocationServices.FusedLocationApi.requestLocationUpdates(LocationModule.this.mGoogleApiClient,
                                 LocationModule.this.mLocationRequest,LocationModule.this);
-                    }catch (SecurityException e){
+                    }catch (Throwable e){
                         Utils.throwError(e);
                     }
 
