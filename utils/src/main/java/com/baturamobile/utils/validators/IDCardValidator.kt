104143999c0e11622c938ce1a379a754e8ebc062
DESKTOP-KARODAP\vssnake <unai@baturamobile.com>
2018-02-28 18:05:11 +0100

    upper case codecard

--- utils/src/main/java/com/baturamobile/utils/validators/IDCardValidator.kt
+++ utils/src/main/java/com/baturamobile/utils/validators/IDCardValidator.kt
@@ -31,7 +31,7 @@ class IDCardValidator private constructor(){
     fun validateIDCARD(isoCountry : String,codeCard: String) : Boolean{
         return try {
             when(COUNTRIES.valueOf(isoCountry)){
-                COUNTRIES.ES -> isSpanishCardValid(codeCard)
+                COUNTRIES.ES -> isSpanishCardValid(codeCard.toUpperCase())
             }
         }catch (e : Exception){
             //If country not listed return true
