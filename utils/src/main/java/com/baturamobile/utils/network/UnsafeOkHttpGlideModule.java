6cf89b864a962654e6b27c52ed027c1cec5a9ff8
DESKTOP-KARODAP\vssnake <unai@baturamobile.com>
2018-03-19 17:18:27 +0100

    glide AppModule to LibraryGlide to remove future issues

--- utils/src/main/java/com/baturamobile/utils/network/UnsafeOkHttpGlideModule.java
+++ utils/src/main/java/com/baturamobile/utils/network/UnsafeOkHttpGlideModule.java
@@ -7,7 +7,7 @@ import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
-import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.module.LibraryGlideModule;
 
 import java.io.InputStream;
 
@@ -18,7 +18,7 @@ import okhttp3.OkHttpClient;
  */
 
 @GlideModule
-public class UnsafeOkHttpGlideModule extends AppGlideModule {
+public class UnsafeOkHttpGlideModule extends LibraryGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
         OkHttpClient client = UnsafeOkHttpClient.getUnsafeOkHttpClient();
